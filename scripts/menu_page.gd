c72c9e9d7f65a28ea947e3b3130d0f5c179daf0a
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-07-03 19:10:47 -0300

    CREATED MENU_PAGE

---
+++ scripts/menu_page.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# Goes to introduction page
+func start_game():
+	get_tree().quit()
+
+# Goes to the options page
+func options_page():
+	get_tree().quit()
+
+# Goes to the credits page
+func credits_page():
+	get_tree().quit()
+	
+# Closes the game
+func exit_game():
+	get_tree().quit()
+
